8a6ecdb59d846ee0ec8f397cc1cc48dcd81a55fb
Aurèle Oulès <aurele@oules.com>
2019-03-12 18:22:16 +0100

    contact

--- src/fragments/Home/index.js
+++ src/fragments/Home/index.js
@@ -7,15 +7,14 @@ class Home extends React.Component {
     render() {
         return <div className="home">
 				<div>
-					<div className="infos">
-						<h1 className="tracking-in-expand">aureleoules</h1>
-                        <h3 className="tracking-in-expand">FullStack Developer</h3>
-					</div>
                     <div className="top">
                         <a className="logo">
                             <img alt="Logo" src={require("../../assets/logo.svg")} />
                         </a>
                         <div className="socials">
+                            <a className="contact " href="mailto:hello@aureleoules.com">
+                                Contact
+                            </a>
                             <a rel="noopener noreferrer" href="https://github.com/aureleoules" target="_blank" >
                                     <img alt="GitHub" src={require("../../assets/icons/github.svg")}/>
                                 </a>
@@ -24,6 +23,10 @@ class Home extends React.Component {
                             </a>
                         </div>
                     </div>
+					<div className="infos">
+						<h1 className="tracking-in-expand">aureleoules</h1>
+                        <h3 className="tracking-in-expand">FullStack Developer</h3>
+					</div>
 				</div>
 			</div>;
     }
