439afa05eb7c2d63fe497030ebf2b156537855d5
Aurèle Oulès <aurele@oules.com>
2019-03-26 17:44:49 +0100

    donation hidden

--- src/fragments/Footer/index.js
+++ src/fragments/Footer/index.js
@@ -9,15 +9,11 @@ class Footer extends React.Component {
         return (
             <footer className="footer">
                 <div className="content">
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/aureleoules/aureleoules3">
-                        <img alt="Logo" src={require("../../assets/logo.svg")} />
+                    <a title={strings.DONATE} target="_blank" rel="noopener noreferrer" href="https://commerce.coinbase.com/checkout/dcb8ffec-8717-4c70-85bb-b0dd588ea4d0">
+                        <img className="animate" alt="Logo" src={require("../../assets/logo.svg")} />
                     </a>
                     <h3>Aurèle Oulès - {new Date().getFullYear()}</h3>
                     <p>{strings.MADE_WITH} <a className="icon-heart">♥</a> {strings.BY} Aurèle Oulès</p>
-                    <div className="donate">
-                        <code>BTC 1LJMmY6RxDJqGEGgVXL7CBaykDFtFHcyNG</code>
-                        <code>XLM GBHZ23YJATFRYHLC3Q6ORM5R525Y5JOKD7KB2KTZVRJT2CZ5RM7N4I2E</code>
-                    </div>
                 </div>
             </footer>
         );
