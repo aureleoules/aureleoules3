6a63eff02be4665f4a0eea180fdb9d8fbbaf1dfd
Aurèle Oulès <aurele@oules.com>
2020-09-26 14:33:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React, {render} from 'preact';
-
-
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
 import './scss/index.scss';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
